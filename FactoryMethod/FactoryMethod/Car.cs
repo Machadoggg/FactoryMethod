5fbb2a12b2c5e421d62b1ea2e953ba299ce56944
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-02 07:21:30 -0500

    Add class Car

---
+++ FactoryMethod/FactoryMethod/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Car : Vehicle
+    {
+        public override int MaxSpeed()
+        {
+            return 240;
+        }
+    }
+}
