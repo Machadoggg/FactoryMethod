bd4874094a31b538928ceb0289581cfa722ec742
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-02 07:33:28 -0500

    Add class Creator and method VehicleCreator

---
+++ FactoryMethod/FactoryMethod/Creator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Creator
+    {
+        public const int MOTORCYCLE = 1;
+        public const int CAR = 2;
+        public const int TRUCK = 3;
+
+        public static Vehicle VehicleCreator(int type)
+        { 
+            switch (type) 
+            { 
+                case MOTORCYCLE:
+                    return new Motorcycle();
+                case CAR: 
+                    return new Car();
+                case TRUCK:
+                    return new Truck();
+                default:
+                    return null;
+            
+            }
+        }
+    }
+}
